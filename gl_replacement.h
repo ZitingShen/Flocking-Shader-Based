344faa44b05e5d2c32ef398dadcf0bd9c008a9bf
Ziting Shen <zshen@brynmawr.edu>
2016-10-17 11:16:17 -0400

    add view and matrix transformation functions definition

--- gl_replacement.h
+++ gl_replacement.h
@@ -14,5 +14,9 @@
 
 void myLookAt(GLfloat eye[3], GLfloat center[3], GLfloat up[3]);
 void myPerspective(GLfloat fovy, GLfloat aspect, GLfloat zNear, GLfloat zFar);
+void myTranslate(GLfloat x, GLfloat y, GLfloat z);
+void myRotate(GLfloat angle, GLfloat x, GLfloat y, GLfloat z);
+void myScale(GLfloat x, GLfloat y, GLfloat z);
+void myMultMatrix(mat4 trans_matrix);
 
 #endif
